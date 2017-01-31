cfca8a6ef78ea50dbad14f66bee19b862664bb58
Fernando Fernandez <ffmp777@yahoo.com>
2017-01-31 07:45:23 -0700

    Routing and views

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
+//* This says "If you visit the home page, a view called 'welcome' will be loaded".  All views are located within resources->views
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); //* In our views this is 'welcome.blade.php' but Laravel does not require us to write '.blade.php' here.
+});
+
+//* Register a new Route:
+Route::get('/about', function() {
+	return view('about');
 });
